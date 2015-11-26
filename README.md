fef5d7eabe14502be99c2588425ebf0640065e01
Austin Weale <austinweale@gmail.com>
2015-11-25 20:08:59 -0800

    finished

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # portfolio
+What is the URL of this project on your student web-server?
+students.washington.edu/alw34/info343/portfolio
+
+Did you receive help from any other sources (classmates, etc.)? If so, please list who.
+no
+
+Approximately how many hours did it take you to complete this challenge?
+11
+
+Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better?
+I feel like it makes sense to use firebase to store data or use the angular multipage architecture but not both. 
+I chose to use multipage architecture to make each tab, and after that it didn't really seem like there was any place to use firebase stored data. So I decided to use firebase to store the tabs data and the state data.  
--- portfolio.js
+++ portfolio.js
@@ -3,41 +3,13 @@ var myApp = angular.module('myApp', ['ui.router', 'firebase']);
 myApp.controller('myCtrl', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject
 ){
 	$scope.ref = new Firebase("https://info343portfolio.firebaseio.com/");
-	//var test = ref.child('Portfolio');
+	
 
 	$scope.data = $firebaseArray($scope.ref);
-
-	$scope.desData = {"description": "", 
-								"link": "",
-								"title":""};
-
-	/*$scope.toggle = function(id){
-		var curr = $scope.ref.child(id);
-
-		$scope.current = $firebaseArray(curr);
-		var i = 0;
-		$scope.current.$loaded().then(function(i){
-			angular.forEach($scope.current, function(key, i) {
-				
-				$scope.desData[key.$id] = key.$value;
-				if(!$scope.$$phase) {
-		          $scope.$apply();
-		        }
-
-			});
-			if(!$scope.$$phase) {
-		          $scope.$apply();
-		    }   
-		});
-
-		if(!$scope.$$phase) {
-          $scope.$apply();
-        }
-	}
-	$scope.toggle("Dawg Coffee");*/
 	
 });
 
+//setting up the different states for the page
 myApp.config(function($stateProvider) {
 	$stateProvider.state('home', { // Landing page
   	url:'/',
